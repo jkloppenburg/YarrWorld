da8bc79e08226b7fc46185aff1b51d0d482a5c8b
jkloppenburg <jkloppenburg1@student.cccd.edu>
2016-09-01 16:30:45 -0700

    Interface

--- app/src/main/java/edu/orangecoastcollege/cs273/jkloppenburg1/yarrworld/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/jkloppenburg1/yarrworld/MainActivity.java
@@ -1,13 +1,22 @@
 package edu.orangecoastcollege.cs273.jkloppenburg1.yarrworld;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Define the botton as an instance variable (member variable)
+    Button speakButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //Restoring user settings on previous settings
         super.onCreate(savedInstanceState);
+        //Inflating (!) opening the layout from activity_main (activity_main.xml)
         setContentView(R.layout.activity_main);
+
+        //Hook up the speakButton to the one in our view:
+        speakButton = (Button) findViewById(R.id.speakButton);
     }
 }
